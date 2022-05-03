c3e81108c110ff963522ffff858514c9532d58c8
carpaltunnel <43389053+carpaltunnel@users.noreply.github.com>
2022-05-02 19:29:46 -0500

    updates

--- controllers/widgets.controllers.js
+++ controllers/widgets.controllers.js
@@ -4,19 +4,27 @@ const createWidget = (req, res, next) => {
 	console.log('widgetsController: createWidget');
 	//console.log(JSON.stringify(req.body));
 	const result = coordinator.createWidget(req.body);
-	res.send(result);
+	res.status(201).json(result);
 };
 
 const getWidgets = (req, res, next) => {
 	//For testing error handler : 
 	//	throw new Error('this stuff is broken');
 	console.log('widgetsController: getWidgets');
-	res.send(coordinator.getWidgets());
+	res.status(200).json(coordinator.getWidgets());
 };
 
 const getWidget = (req, res, next) => {
 	console.log(`widgetsController: getWidget(${req.params.id})`);
-	res.send(coordinator.getWidget(req.params.id));
+
+	const widget = coordinator.getWidget(req.params.id);
+
+	// If widget with ID was found in database
+	if (widget) {
+		res.status(200).json(widget);
+	} else {
+		res.status(404).send();
+	}
 };
 
 const updateWidget = (req, res, next) => {
